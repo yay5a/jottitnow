fc9591ea91060fb9bf196314aab116611a8d203b
Chartok <m.bhimjee@outlook.com>
2023-05-23 20:12:13 -0500

    squashed route bug

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -2,13 +2,13 @@ const path = require('path');
 const router = require('express').Router();
 
 // GET homepage to jott notes
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/jott.html'));
+router.get('/jott', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/jott.html'));
 });
 
 // '*' wildcard route to index.html
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const indexRoutes = require('/routes/indexRoutes');
-const jottRoutes = require('/routes/jottRoutes');
+const indexRoutes = require('./routes/indexRoutes');
+const jottRoutes = require('./routes/jottRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
