78c4ab0f339ceffa16ba2af55d20fc808e28c2d8
Chartok <m.bhimjee@outlook.com>
2023-05-27 19:57:06 -0500

    more typo fixes

--- routes/jottRoutes.js
+++ routes/jottRoutes.js
@@ -2,25 +2,25 @@ const router = require('express').Router();
 const store = require('../db/store');
 
 // GET route to display notes on homepage
-router.get('/jott', (req, res) => {
+router.get('/jotts', (req, res) => {
   store
     .getJotts()
-    .then((jott) => {
-      return res.json(jott);
+    .then((jotts) => {
+      return res.json(jotts);
     })
     .catch((err) => res.status(500).json(err));
 });
 
 // POST route to jott new notes and store them in local storage
-router.post('/jott', (req, res) => {
+router.post('/jotts', (req, res) => {
   store
     .addJott(req.body)
-    .then((jott) => res.json())
+    .then((jott) => res.json(jott))
     .catch((err) => res.status(500).json(err));
 });
 
 // DELETE route to delete notes
-router.delete('/jott/:id', (req, res) => {
+router.delete('/jotts/:id', (req, res) => {
   store
     .removeJott(req.params.id)
     .then(() => res.json({ ok: true }))
