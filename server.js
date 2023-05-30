93187c6af6aa59a870b2c73c252c3a04e16d47a8
Chartok <m.bhimjee@outlook.com>
2023-05-29 21:07:51 -0500

    refactored server and routes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use('/jott', jottRoutes);
+app.use('/api/jottRoutes', jottRoutes);
 app.use('/', indexRoutes);
 
 
