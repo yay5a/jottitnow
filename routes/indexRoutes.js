5d26bed88c695de109c5080c495fa985a3e1e7ba
Chartok <m.bhimjee@outlook.com>
2023-05-29 21:51:47 -0500

    refactored server and routes

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -4,7 +4,7 @@ const path = require('path');
 const router = express.Router();
 
 // GET homepage to display jott notes
-router.get('/api/jotts', (req, res) => {
+router.get('/jotts', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/jotts.html'));
 });
 
